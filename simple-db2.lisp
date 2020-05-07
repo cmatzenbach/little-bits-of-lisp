bc279759e54df2d3b27af4dfc99dd7eef8b6b7df
cmatzenbach <cmatzenbach@gmail.com>
2020-05-07 01:04:41 -0500

    Added function for user to input cd data through terminal question mode

--- simple-db2.lisp
+++ simple-db2.lisp
@@ -21,8 +21,10 @@
 
 ;; make new cd record by prompting users for info
 (defun prompt-for-cd ()
-  (prompt-read "Title")
-  (prompt-read "Artist")
-  (prompt-read "Rating")
-  (prompt-read "Ripped"))
+  (make-cd
+   (prompt-read "Title")
+   (prompt-read "Artist")
+   (or (parse-integer (prompt-read "Rating") :junk-allowed t) 0)
+   (y-or-n-p "Ripped [y/n]: ")))
+
 
